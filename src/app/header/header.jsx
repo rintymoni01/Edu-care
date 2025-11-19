42e04250db00e77777e85128bc719705728ab2fe
rintymoni01 <rintymoni01@gmail.com>
2025-11-19 20:06:24 +0600

    My New Project Add

---
+++ src/app/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <p>i love me</p>
+        </div>
+    );
+};
+
+export default header;
---
+++ src/app/home/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const home = () => {
+    return (
+        <div>
+            <h1>hi am riny</h1>
+        </div>
+    );
+};
+
+export default home;
